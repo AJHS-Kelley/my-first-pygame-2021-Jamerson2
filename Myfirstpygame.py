79cb9c0a9ee48f96f4f22f5986a9f35c13e3f581
Jamerson f <90413446+Jamerson2@users.noreply.github.com>
2021-11-30 14:39:20 -0500

    v.3
    add color

--- Myfirstpygame.py
+++ Myfirstpygame.py
@@ -1,4 +1,4 @@
-# My first pygame, Jamerson Franklin, 11/30/21, 2:13pm, vo.2
+# My first pygame, Jamerson Franklin, 11/30/21, 2:13pm, vo.3
 
 import pygame, sys
 from pygame.locals import *
@@ -9,3 +9,9 @@ pygame.init()
 # setup the game window
 windowsurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('Hello, world!')
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+DOODOOBROWN = (125, 255, 25)
